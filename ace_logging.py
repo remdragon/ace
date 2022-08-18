4ab639528bdf9a12102e381ad9d0ac48a76bfaeb
Royce Mitchell <3742423+remdragon@users.noreply.github.com>
2022-08-18 17:53:51 -0500

    attempt to fix linux logging issue

--- ace_logging.py
+++ ace_logging.py
@@ -7,18 +7,18 @@ if sys.platform != 'win32':
 	from systemd.journal import JournaldLogHandler # pip install systemd
 
 def init( loglevels: Dict[str,str] ) -> None:
+	logging.basicConfig(
+		level = logging.DEBUG,
+		#level = DEBUG9,
+		format = '%(asctime)s:%(levelname)s:%(name)s:%(message)s',
+	)
+	
 	if sys.platform != 'win32':
 		journald_handler = JournaldLogHandler()
 		journald_handler.setFormatter(
 			logging.Formatter( '[%(levelname)s] %(message)s' )
 		)
 		logging.getLogger( '' ).addHandler( journald_handler )
-	
-	logging.basicConfig(
-		level = logging.DEBUG,
-		#level = DEBUG9,
-		format = '%(asctime)s:%(levelname)s:%(name)s:%(message)s',
-	)
 	
 	for name, level in loglevels.items():
 		assert level.isnumeric() or level in ( 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL' ), f'invalid level={level!r}'
