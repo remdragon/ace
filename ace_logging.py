87228dc217bd0556182c91743bc25270b89daf19
Royce Mitchell <3742423+remdragon@users.noreply.github.com>
2022-08-26 12:45:56 -0500

    work around bug in JournaldLogHandler

--- ace_logging.py
+++ ace_logging.py
@@ -4,7 +4,11 @@ import sys
 from typing import Dict
 
 if sys.platform != 'win32':
-	from systemd.journal import JournaldLogHandler # pip install systemd
+	try:
+		from systemd.journal import JournaldLogHandler # pip install systemd
+	except ImportError:
+		print( 'WARNING: JournaldLogHandler not found', file = sys.stderr )
+		JournaldLogHandler = None
 
 def init( loglevels: Dict[str,str] ) -> None:
 	logging.basicConfig(
@@ -13,7 +17,7 @@ def init( loglevels: Dict[str,str] ) -> None:
 		format = '%(asctime)s:%(levelname)s:%(name)s:%(message)s',
 	)
 	
-	if sys.platform != 'win32':
+	if sys.platform != 'win32' and JournaldLogHandler:
 		journald_handler = JournaldLogHandler()
 		journald_handler.setFormatter(
 			logging.Formatter( '[%(levelname)s] %(message)s' )
